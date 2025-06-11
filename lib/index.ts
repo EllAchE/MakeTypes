de2f31d535067dad60c20e4e59489a75d66e67b9
EllAchE <26192612+EllAchE@users.noreply.github.com>
2025-06-11 04:37:09 -0700

    remove yargs

--- lib/index.ts
+++ lib/index.ts
@@ -2,11 +2,11 @@
  * Library entry point. Exports public-facing interfaces.
  */
 
-import Writer from './writer';
 import CbWriter from './cb_writer';
+import { default as Emitter } from './emit';
 import NopWriter from './nop_writer';
 import StreamWriter from './stream_writer';
 import * as Types from './types';
-import {default as Emitter} from './emit';
+import Writer from './writer';
 
-export {Writer, CbWriter, NopWriter, StreamWriter, Types, Emitter};
+export { CbWriter, Emitter, NopWriter, StreamWriter, Types, Writer };
